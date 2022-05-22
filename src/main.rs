db98ed4d8874d1be32cf03cf3de44a809b0302ac
Mos Alem <mostafaalemzadeh@gmail.com>
2022-05-22 19:16:12 +0430

    Adding some functionality

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,54 @@
 use std::convert::Infallible;
 use std::net::SocketAddr;
-use hyper::{Body, Request, Response, Server};
+use hyper::{Body, Request, Response, Server, Method, StatusCode, };
 use hyper::service::{make_service_fn, service_fn};
+use futures::TryStreamExt as _;
 
-async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new("Hello, World".into()))
+
+async fn echo(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    let mut response = Response::new(Body::empty());
+
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => {
+            *response.body_mut() = Body::from("Try POSTing data to /echo");
+        },
+        (&Method::POST, "/echo") => {
+            *response.body_mut() = req.into_body();
+        },
+        // Yet another route inside our match block...
+        (&Method::POST, "/echo/uppercase") => {
+            // This is actually a new `futures::Stream`...
+            let mapping = req
+                .into_body()
+                .map_ok(|chunk| {
+                    chunk.iter()
+                        .map(|byte| byte.to_ascii_uppercase())
+                        .collect::<Vec<u8>>()
+                });
+
+
+            // Use `Body::wrap_stream` to convert it to a `Body`...
+            *response.body_mut() = Body::wrap_stream(mapping);
+        },
+        // Yet another route inside our match block...
+        (&Method::POST, "/echo/reverse") => {
+            // Await the full body to be concatenated into a single `Bytes`...
+            let full_body = hyper::body::to_bytes(req.into_body()).await?;
+
+            // Iterate the full body in reverse order and collect into a new Vec.
+            let reversed = full_body.iter()
+                .rev()
+                .cloned()
+                .collect::<Vec<u8>>();
+
+            *response.body_mut() = reversed.into();
+        },
+        _ => {
+            *response.status_mut() = StatusCode::NOT_FOUND;
+        },
+    };
+
+    Ok(response)
 }
 
 #[tokio::main]
@@ -16,7 +60,7 @@ async fn main() {
     // creates one from our `hello_world` function.
     let make_svc = make_service_fn(|_conn| async {
         // service_fn converts our function into a `Service`
-        Ok::<_, Infallible>(service_fn(hello_world))
+        Ok::<_, Infallible>(service_fn(echo))
     });
 
     let server = Server::bind(&addr).serve(make_svc);
