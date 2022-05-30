e3cc471f4bf0ba65dc41ef4a4442e26216220da9
Mos Alem <mostafaalemzadeh@gmail.com>
2022-05-30 14:23:30 +0430

    Start to modularize the app into separate files.

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,18 @@
+
 use std::convert::Infallible;
 use std::net::SocketAddr;
-use hyper::{Body, Request, Response, Server, Method, StatusCode, };
+use hyper::{Server};
 use hyper::service::{make_service_fn, service_fn};
-use futures::TryStreamExt as _;
+use rnet::handlers::echo::echo;
+
+
+
 
 async fn shutdown_signal() {
     // Wait for the CTRL+C signal
     tokio::signal::ctrl_c()
         .await
         .expect("failed to install CTRL+C signal handler");
-}
-
-async fn echo(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-    let mut response = Response::new(Body::empty());
-
-    match (req.method(), req.uri().path()) {
-        (&Method::GET, "/") => {
-            *response.body_mut() = Body::from("Try POSTing data to /echo");
-        },
-        (&Method::POST, "/echo") => {
-            *response.body_mut() = req.into_body();
-        },
-        // Yet another route inside our match block...
-        (&Method::POST, "/echo/uppercase") => {
-            // This is actually a new `futures::Stream`...
-            let mapping = req
-                .into_body()
-                .map_ok(|chunk| {
-                    chunk.iter()
-                        .map(|byte| byte.to_ascii_uppercase())
-                        .collect::<Vec<u8>>()
-                });
-
-
-            // Use `Body::wrap_stream` to convert it to a `Body`...
-            *response.body_mut() = Body::wrap_stream(mapping);
-        },
-        // Yet another route inside our match block...
-        (&Method::POST, "/echo/reverse") => {
-            // Await the full body to be concatenated into a single `Bytes`...
-            let full_body = hyper::body::to_bytes(req.into_body()).await?;
-
-            // Iterate the full body in reverse order and collect into a new Vec.
-            let reversed = full_body.iter()
-                .rev()
-                .cloned()
-                .collect::<Vec<u8>>();
-
-            *response.body_mut() = reversed.into();
-        },
-        _ => {
-            *response.status_mut() = StatusCode::NOT_FOUND;
-        },
-    };
-
-    Ok(response)
 }
 
 #[tokio::main]
